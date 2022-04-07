eb8ea45fc2e68d8fb3843cd4bf2975cdaa4d027b
Kristina Totera <ktotera@boweryfarming.com>
2022-04-07 16:01:04 -0400

    make generic

--- search-engine/src/components/Header.tsx
+++ search-engine/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from 'react'
 const Header = (): ReactElement => {
     return (
         <div className='h-[20vh] bg-orange-300 flex flex-col items-center justify-center font-serif text-3xl'>
-            Morning Brew Dad Jokes!
+            Dad Jokes!
         </div>
     )
 }
